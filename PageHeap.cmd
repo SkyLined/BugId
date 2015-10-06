dbfe62d6e43c24d277b7dd57c9a413d1fe6d289a
SkyLined <berendjanwever@gmail.com>
2015-10-06 22:10:14 +0200

    Improve PageHeap.cmd

--- PageHeap.cmd
+++ PageHeap.cmd
@@ -12,13 +12,24 @@ IF NOT "%GFlags:~0,0%" == "" (
     SET GFlags=C:\Program Files\Windows Kits\8.1\Debuggers\x86\gflags.exe
   )
 )
-IF NOT EXIST "%GFlags:"=%" (
-  ECHO - Cannot find Global Flags at %GFlags%, please set the "GFlags" environment variable to the correct path.
+SET GFlags=%GFlags:"=%
+IF NOT EXIST "%GFlags%" (
+  ECHO - Cannot find Global Flags at "%GFlags%", please set the "GFlags" environment variable to the correct path.
   EXIT /B 1
 )
 IF "%~2" == "OFF" (
-  "%GFlags%" -i %1 -FFFFFFFF
-) ELSE (
+  ECHO * Disabling page heap for %1...
   "%GFlags%" -i %1 -FFFFFFFF >nul
-  "%GFlags%" -i %1 +02109870
+  IF ERRORLEVEL 1 GOTO :ERROR
+) ELSE (
+  ECHO * Enabling page heap for %1...
+  "%GFlags%" -i %1 -FFFFFFFF >nul
+  IF ERRORLEVEL 1 GOTO :ERROR
+  "%GFlags%" -i %1 +02109870 >nul
+  IF ERRORLEVEL 1 GOTO :ERROR
 )
+EXIT /B 0
+
+:ERROR
+  ECHO - Error code %ERRORLEVEL%.
+  EXIT /B %ERRORLEVEL%
