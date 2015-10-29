a3f7bce061cd3b7fe8a8fcecf6278e4e44be7059
SkyLined <berendjanwever@gmail.com>
2015-10-30 00:18:42 +0100

    Minor bug fix + extra comments

--- src/cBugId.py
+++ src/cBugId.py
@@ -3,13 +3,17 @@ from cCdbWrapper import cCdbWrapper;
 
 class cBugId(object):
   def __init__(oBugId, **dxArguments):
+    # Replace fFinishedCallback with a wrapper that signals the finished event.
+    # This event is used by the fWait function to wait for the process to
+    # finish.
     oBugId.__fExternalFinishedCallback = dxArguments.get("fFinishedCallback");
     dxArguments["fFinishedCallback"] = oBugId.__fInternalFinishedHandler;
     oBugId.__oFinishedEvent = threading.Event();
+    # Run the application in a debugger and catch exceptions.
     oBugId.__oCdbWrapper = cCdbWrapper(**dxArguments);
   
   def fStop(oBugId):
-    oBugId.__oCdbWrapper.fStop();
+    oBugId.__oCdbWrapper and oBugId.__oCdbWrapper.fStop();
   
   def fWait(oBugId):
     oBugId.__oFinishedEvent.wait();
