5821c17a937a2ab08ad6509475b00380a9295c03
SkyLined <berendjanwever@gmail.com>
2016-03-03 13:57:54 +0100

    Add "bSaveTestReports" setting, default to False

--- dxConfig.py
+++ dxConfig.py
@@ -2,6 +2,7 @@ import os;
 sBaseFolderPath = os.path.dirname(__file__);
 dxConfig = {
   "bSaveReport": True,          # Have BugId.py output a HTML formatted crash report.
+  "bSaveTestReports": False,    # Have Tests.py output a HTML formatted crash report.
   "sKillBinaryPath_x86": os.path.join(sBaseFolderPath, "modules", "Kill", "bin", "Kill_x86.exe"),
   "sKillBinaryPath_x64": os.path.join(sBaseFolderPath, "modules", "Kill", "bin", "Kill_x64.exe"),
 };
