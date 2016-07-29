27bf591f4c895bc4ad64bf5aa80ffe8dd436dc80
SkyLined <berendjanwever@gmail.com>
2016-07-29 10:48:46 +0200

    Cleanup dxConfig documentation

--- dxConfig.py
+++ dxConfig.py
@@ -1,11 +1,15 @@
 import os;
 sBaseFolderPath = os.path.dirname(__file__);
 dxConfig = {
-  "bSaveReport": True,          # Have BugId.py output a HTML formatted crash report.
-  "asSymbolServerURLs": ["http://msdl.microsoft.com/download/symbols"],
-  "nExcessiveCPUUsageCheckInitialTimeout": 5, # Start checking the application for excessive CPU usage
-                                                    # after this many seconds.
-  "sTest": "http://%s:28876/" % os.getenv("COMPUTERNAME"), # Default URL for browser tests, can be a file path for other applications.
+  "bSaveReport": True,                            # Set to True to have BugId.py output a HTML formatted crash report.
+  "asSymbolServerURLs": [                         # List of symbol server URLs for use by cdb.
+    "http://msdl.microsoft.com/download/symbols"
+  ],
+  "nExcessiveCPUUsageCheckInitialTimeout": 5,     # Start checking the application for excessive CPU usage after this
+                                                  # many seconds. Lower values yield results faster, but slow down
+                                                  # testing and may give false positives if startup takes long.
+  "sTest": "http://%s:28876/" % os.getenv("COMPUTERNAME"), # Default URL for browser tests, can be a file path for
+                                                  # applications that read test data from a file.
   "BugId": {
     # The values from src\dxBugIdConfig.py get loaded here, but you can override them here.
   },
