9fc6f119f819176bcb24e5c424109ba32d01cbaf
SkyLined <berendjanwever@gmail.com>
2016-08-03 13:00:05 +0200

    Yet another attempt to fix a bug in PageHeap.cmd
    * Running the script with GFlags set and a "(" in its value caused an
    error. I'm hoping this is now resolved

--- PageHeap.cmd
+++ PageHeap.cmd
@@ -6,23 +6,25 @@ IF ERRORLEVEL 1 (
   EXIT /B 1
 )
 IF NOT DEFINED GFlags (
-  CALL :SET_GFLAGS
+  CALL :SET_GFLAGS_%PROCESSOR_ARCHITECTURE%
+) ELSE (
+  :: Make sure GFlags is quoted
+  SET GFlags="%GFlags:"=%"
 )
-SET GFlags=%GFlags:"=%
 
-IF NOT EXIST "%GFlags%" (
-  ECHO - Cannot find gflags.exe at "%GFlags%", please set the "GFlags" environment variable to the correct path.
+IF NOT EXIST %GFlags% (
+  ECHO - Cannot find gflags.exe at %GFlags%, please set the "GFlags" environment variable to the correct path.
   EXIT /B 1
 )
 IF "%~2" == "OFF" (
   ECHO * Disabling page heap for %1...
-  "%GFlags%" -i %1 -FFFFFFFF >nul
+  %GFlags% -i %1 -FFFFFFFF >nul
   IF ERRORLEVEL 1 GOTO :ERROR
 ) ELSE IF "%~2" == "ON" (
   ECHO * Enabling page heap for %1...
-  "%GFlags%" -i %1 -FFFFFFFF >nul
+  %GFlags% -i %1 -FFFFFFFF >nul
   IF ERRORLEVEL 1 GOTO :ERROR
-  "%GFlags%" -i %1 +02109870 >nul
+  %GFlags% -i %1 +02109870 >nul
   IF ERRORLEVEL 1 GOTO :ERROR
 ) ELSE (
   ECHO Usage:
@@ -30,17 +32,25 @@ IF "%~2" == "OFF" (
 )
 EXIT /B 0
 
-:SET_GFLAGS
-  IF "%PROCESSOR_ARCHITECTURE%" == "AMD64" (
-    SET GFlags=C:\Program Files\Windows Kits\8.1\Debuggers\x64\gflags.exe
-  ) ELSE (
-    SET GFlags=C:\Program Files\Windows Kits\8.1\Debuggers\x86\gflags.exe
-  )
-  IF NOT EXIST "%GFlags%" (
-    IF "%PROCESSOR_ARCHITECTURE%" == "AMD64" (
-      SET GFlags=C:\Program Files\Windows Kits\10\Debuggers\x64\gflags.exe
-    ) ELSE (
-      SET GFlags=C:\Program Files\Windows Kits\10\Debuggers\x86\gflags.exe
+:SET_GFLAGS_AMD64
+  CALL :SET_GFLAGS_IF_EXISTS "%ProgramFiles%\Windows Kits\10\Debuggers\x64\gflags.exe"
+  CALL :SET_GFLAGS_IF_EXISTS "%ProgramFiles%\Windows Kits\8.1\Debuggers\x64\gflags.exe"
+  CALL :SET_GFLAGS_IF_EXISTS "%ProgramFiles%\Windows Kits\8.0\Debuggers\x64\gflags.exe"
+  EXIT /B 0
+
+:SET_GFLAGS_x86
+  CALL :SET_GFLAGS_IF_EXISTS "%ProgramFiles%\Windows Kits\10\Debuggers\x86\gflags.exe"
+  CALL :SET_GFLAGS_IF_EXISTS "%ProgramFiles(x86)%\Windows Kits\10\Debuggers\x86\gflags.exe"
+  CALL :SET_GFLAGS_IF_EXISTS "%ProgramFiles%\Windows Kits\8.1\Debuggers\x86\gflags.exe"
+  CALL :SET_GFLAGS_IF_EXISTS "%ProgramFiles(x86)%\Windows Kits\8.1\Debuggers\x86\gflags.exe"
+  CALL :SET_GFLAGS_IF_EXISTS "%ProgramFiles%\Windows Kits\8.0\Debuggers\x86\gflags.exe"
+  CALL :SET_GFLAGS_IF_EXISTS "%ProgramFiles(x86)%\Windows Kits\8.0\Debuggers\x86\gflags.exe"
+  EXIT /B 0
+
+:SET_GFLAGS_IF_EXISTS
+  IF NOT DEFINED GFlags (
+    IF EXIST "%~1" (
+      SET GFlags="%~1"
     )
   )
   EXIT /B 0
