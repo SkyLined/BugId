99b6a67d20b348379d9fcc1cb9d6a4c0e463e943
SkyLined <berendjanwever@gmail.com>
2016-08-04 17:27:39 +0200

    Add cdb wrapper for starting bugid.

---
+++ BugId.cmd
@@ -0,0 +1,14 @@
+@ECHO OFF
+SETLOCAL
+IF NOT DEFINED PYTHON (
+  SET PYTHON="%SystemDrive%:\Python27\python.exe"
+) ELSE (
+  SET PYTHON="%PYTHON:"=%"
+)
+IF NOT EXIST %PYTHON% (
+  ECHO - Cannot find Python at %PYTHON%, please set the "PYTHON" environment variable to the correct path.
+  EXIT /B 1
+)
+
+%PYTHON% "%~dpn0.py" %*
+EXIT /B %ERRORLEVEL%
