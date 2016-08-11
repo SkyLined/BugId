a6c72d1cda71f792a614f598c885706ed1853c61
SkyLined <berendjanwever@gmail.com>
2016-08-11 13:24:17 +0200

    Add Apache OpenOffice Writer keyword

--- cBugId/cCdbWrapper_fCdbInterruptOnTimeoutThread.py
+++ cBugId/cCdbWrapper_fCdbInterruptOnTimeoutThread.py
@@ -14,18 +14,18 @@ def cCdbWrapper_fCdbInterruptOnTimeoutThread(oCdbWrapper):
       if not oCdbWrapper.bInterruptPending:
         # Time spent running before the application was resumed + time since the application was resumed.
         nApplicationRunTime = oCdbWrapper.fnApplicationRunTime();
-#        print "@@@ application run time    : %.3f" % nApplicationRunTime;
-#        print "@@@ number of timeouts      : %d" % len(oCdbWrapper.axTimeouts);
+        print "@@@ application run time    : %.3f" % nApplicationRunTime;
+        print "@@@ number of timeouts      : %d" % len(oCdbWrapper.axTimeouts);
         oCdbWrapper.oTimeoutsLock.acquire();
         for (nTimeoutTime, fTimeoutCallback, axTimeoutCallbackArguments) in oCdbWrapper.axTimeouts: # Make a copy so modifcation during the loop does not affect it.
           if nTimeoutTime <= nApplicationRunTime:
             # If there was a timeout, and there is no interrupt pending, interrupt cdb.
             oCdbWrapper.bInterruptPending = True;
             oCdbWrapper.oCdbProcess.send_signal(signal.CTRL_BREAK_EVENT);
-#            print "@@@ timeout for %.3f => %s" % (nTimeoutTime, repr(fTimeoutCallback));
+            print "@@@ timeout for %.3f => %s" % (nTimeoutTime, repr(fTimeoutCallback));
             break;
           else:
-#            print "@@@ sleep for %.3f => %s" % (nTimeoutTime, repr(fTimeoutCallback));
+            print "@@@ sleep for %.3f => %s" % (nTimeoutTime, repr(fTimeoutCallback));
             pass;
         oCdbWrapper.oTimeoutsLock.release();
     finally:
