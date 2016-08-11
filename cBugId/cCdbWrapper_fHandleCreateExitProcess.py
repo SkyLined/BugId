8f6acb9b92fadcb749e3bef6744533634a6cdddd
SkyLined <berendjanwever@gmail.com>
2016-08-11 13:10:20 +0200

    Output process creation and termination to report

--- cBugId/cCdbWrapper_fHandleCreateExitProcess.py
+++ cBugId/cCdbWrapper_fHandleCreateExitProcess.py
@@ -1,6 +1,15 @@
 import re;
 from dxBugIdConfig import dxBugIdConfig;
 
+def fDebugOutputProcesses(oCdbWrapper, sMessage, *auArguments):
+  asDebugOutput = oCdbWrapper.fasSendCommandAndReadOutput(
+    '.printf "%s\\r\\n";' % sMessage,
+    bShowOnlyCommandOutput = True,
+  );
+  if not oCdbWrapper.bCdbRunning: return;
+  assert len(asDebugOutput) == 1, "Unexpected output: %s" % repr(asDebugOutput);
+  if dxBugIdConfig["bOutputProcesses"]: "@@@ %s" % asDebugOutput[0];
+
 def cCdbWrapper_fHandleNewProcess(oCdbWrapper, uProcessId):
   # If cdb has created this process (i.e. it is not attaching to any process ids), this is the "main" process. If
   # fApplicationExitCallback is provided, we need to detect when the main process exists.
@@ -14,12 +23,10 @@ def cCdbWrapper_fHandleNewProcess(oCdbWrapper, uProcessId):
     uPendingAttachProcessId = oCdbWrapper.auProcessIdsPendingAttach.pop(0);
     assert uPendingAttachProcessId == uProcessId, \
         "Expected to attach to process %d, got %d" % (uPendingAttachProcessId, uProcessId);
-    if dxBugIdConfig["bOutputProcesses"]:
-      print "* Attached to process %d/0x%X." % (uProcessId, uProcessId);
+    fDebugOutputProcesses(oCdbWrapper, "* Attached to process %d/0x%X." % (uProcessId, uProcessId));
     bInitialProcessesCreated = len(oCdbWrapper.auProcessIdsPendingAttach) == 0;
   else:
-    if dxBugIdConfig["bOutputProcesses"]:
-      print "* New process %d/0x%X." % (uProcessId, uProcessId);
+    fDebugOutputProcesses(oCdbWrapper, "* New process %d/0x%X." % (uProcessId, uProcessId));
     # Make sure all child processes of this process are debugged as well.
   # This may be superfluous, as I believe this is a global flag, not per-process, but it should have negligable
   # affect on performance and would prevent bugs if this assumption is not true.
@@ -40,7 +47,6 @@ def cCdbWrapper_fHandleCreateExitProcess(oCdbWrapper, sCreateExit, uProcessId):
     if oCdbWrapper.fApplicationExitCallback and (uProcessId in oCdbWrapper.auMainProcessIds):
       oCdbWrapper.fApplicationExitCallback();
     oCdbWrapper.uLastProcessId = uProcessId;
-    if dxBugIdConfig["bOutputProcesses"]:
-      print "* Terminated process %d/0x%X." % (uProcessId, uProcessId);
+    fDebugOutputProcesses(oCdbWrapper, "* Terminated process %d/0x%X." % (uProcessId, uProcessId));
   else:
     raise AssertionError("Unknown sCreateExit value %s" % sCreateExit);
