446af3d4eb8140bc81a3f926d90730d4979cfa8b
SkyLined <berendjanwever@gmail.com>
2016-09-27 09:56:36 +0200

    Pageheap for Chrome improved
    * Chrome can run using page heap if you set an environment variable.
    This is now implemented.

--- PageHeap-Chrome.cmd
+++ PageHeap-Chrome.cmd
@@ -1,5 +1,11 @@
 @ECHO OFF
 IF "%~1" == "ON" (
-  ECHO NOTE: Chrome has its own heap manager. Turning page heap ON will not benefit you.
+  REG ADD "HKCU\Environment" /v "CHROME_ALLOCATOR" /t REG_SZ /d "winheap" /f
+  SET CHROME_ALLOCATOR=winheap
+) ELSE (
+  REG DEL "HKCU\Environment" /v "CHROME_ALLOCATOR" /f
+  SET CHROME_ALLOCATOR=
 )
 CALL PageHeap.cmd chrome.exe %1
+ECHO Note that page heap for Chrome requires an environment variable
+ECHO to be set. You may need to reboot to make sure this is done.
