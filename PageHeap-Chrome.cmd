00f948e7b44f8d74fcc7a9578e407412322125d5
SkyLined <berendjanwever@gmail.com>
2016-09-27 12:10:28 +0200

    Improve, fix bug
    * REG DEL does work, REG DELETE does.
    * Better documentation of env.var. problems

--- PageHeap-Chrome.cmd
+++ PageHeap-Chrome.cmd
@@ -1,11 +1,17 @@
 @ECHO OFF
 IF "%~1" == "ON" (
-  REG ADD "HKCU\Environment" /v "CHROME_ALLOCATOR" /t REG_SZ /d "winheap" /f
   SET CHROME_ALLOCATOR=winheap
+  REG ADD "HKCU\Environment" /v "CHROME_ALLOCATOR" /t REG_SZ /d "winheap" /f
 ) ELSE (
-  REG DEL "HKCU\Environment" /v "CHROME_ALLOCATOR" /f
   SET CHROME_ALLOCATOR=
+  REG DELETE "HKCU\Environment" /v "CHROME_ALLOCATOR" /f
+)
+IF ERRORLEVEL 1 (
+  ECHO - Cannot set CHROME_ALLOCATOR enironment variable in the registry.
 )
 CALL PageHeap.cmd chrome.exe %1
-ECHO Note that page heap for Chrome requires an environment variable to be set.
-ECHO You may need to log off and on again to make sure this is done.
+ECHO Note: switching page heap on or off for Chrome requires the CHROME_ALLOCATOR
+ECHO environment variable to be set to "winheap" or be unset respectively. This
+ECHO change has been applied through the registry and in the current process'
+ECHO environment, but you may want to log off and on again to make sure this is
+ECHO applied to all processes.
