d11667b93bfb4e0c51d2b331e3f1ae444ec7ac5b
SkyLined <berendjanwever@gmail.com>
2016-09-27 09:59:16 +0200

    Update explanation

--- PageHeap-Chrome.cmd
+++ PageHeap-Chrome.cmd
@@ -7,5 +7,5 @@ IF "%~1" == "ON" (
   SET CHROME_ALLOCATOR=
 )
 CALL PageHeap.cmd chrome.exe %1
-ECHO Note that page heap for Chrome requires an environment variable
-ECHO to be set. You may need to reboot to make sure this is done.
+ECHO Note that page heap for Chrome requires an environment variable to be set.
+ECHO You may need to log off and on again to make sure this is done.
