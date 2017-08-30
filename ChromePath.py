5ac8e5794ec5335e84d0ea4df67ca82288a81bec
SkyLined <github@skylined.nl>
2017-08-30 15:30:18 +0200

    Do not use chrome-sxs_* for chrome_*
    + `chrome_*` will get you `chrome_*` or nothing
    + `chrome` will still get you `chrome_*` or `chrome-sxs_*`
    + On x64 systems, assume Chrome in `Program Files (x86)` is x64, as I
    found this to be where it is installed.

--- ChromePath.py
+++ ChromePath.py
@@ -5,7 +5,7 @@ sProgramFilesPath_x86 = os.getenv("ProgramFiles(x86)") or os.getenv("ProgramFile
 sProgramFilesPath_x64 = os.getenv("ProgramW6432");
 sLocalAppData = os.getenv("LocalAppData");
 
-# Chrome Canary
+# Chrome Canary (if installed)
 sChromeSxSPath_x64 = sProgramFilesPath_x64 and fsFirstExistingFile(
   r"%s\Google\Chrome SxS\Application\chrome.exe" % sProgramFilesPath_x64,
   r"%s\Google\Chrome SxS\Application\chrome.exe" % sLocalAppData,
@@ -15,15 +15,18 @@ sChromeSxSPath_x86 = fsFirstExistingFile(
   r"%s\Google\Chrome SxS\Application\chrome.exe" % sLocalAppData,
 );
 sChromeSxSPath = sChromeSxSPath_x64 or sChromeSxSPath_x86;
-# Chrome stable (if installed, otherwise use Chrome Canary if installed).
+# Chrome stable (if installed)
 sChromePath_x64 = sProgramFilesPath_x64 and fsFirstExistingFile(
   r"%s\Google\Chrome\Application\chrome.exe" % sProgramFilesPath_x64,
   r"%s\Google\Chrome\Application\chrome.exe" % sLocalAppData,
-  sChromeSxSPath_x64,
 );
+if os.getenv("ProgramFiles(x86)"):
+  # on x64 systems, x64 versions of Chrome can be installed in the x86 Program Files folder...
+  sChromePath_x64 = sChromePath_x64 or fsFirstExistingFile(
+    r"%s\Google\Chrome\Application\chrome.exe" % sProgramFilesPath_x86,
+  );
 sChromePath_x86 = fsFirstExistingFile(
   r"%s\Google\Chrome\Application\chrome.exe" % sProgramFilesPath_x86,
   r"%s\Google\Chrome\Application\chrome.exe" % sLocalAppData,
-  sChromeSxSPath_x86,
 );
 sChromePath = sChromePath_x64 or sChromePath_x86 or sChromeSxSPath;
