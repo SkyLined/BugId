f5de287bc6bfb09fa542b03153515f96a4a89a42
SkyLined <github@skylined.nl>
2017-10-21 20:39:06 +0200

    Remove static module

--- modules/mFileSystem/modules/mWindowsAPI/ADVAPI32.py
+++
@@ -1,8 +0,0 @@
-from cDLL import cDLL;
-from PrimitiveTypes import *;
-from StructureTypes import *;
-
-ADVAPI32 = cDLL("Advapi32.dll");
-ADVAPI32.fDefineFunction(BOOL, "GetTokenInformation", HANDLE, TOKEN_INFORMATION_CLASS, LPVOID, DWORD, PDWORD);
-ADVAPI32.fDefineFunction(PUCHAR, "GetSidSubAuthorityCount", PSID);
-ADVAPI32.fDefineFunction(PDWORD, "GetSidSubAuthority", PSID, DWORD);
--- modules/mFileSystem/modules/mWindowsAPI/__init__.py
+++
@@ -1,9 +0,0 @@
-from Defines import *;
-from Functions import *;
-from PrimitiveTypes import *;
-from StructureTypes import *;
-
-from ADVAPI32 import ADVAPI32;
-from KERNEL32 import KERNEL32;
-
-from oVersionInformation import oVersionInformation;
