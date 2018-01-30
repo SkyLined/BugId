f9beb0736e5f4bccf96b130bc0120564b5c33a0c
SkyLined <github@skylined.nl>
2018-01-30 23:55:12 +0100

    Improve tests

--- Tests.cmd
+++ Tests.cmd
@@ -2,9 +2,33 @@
 
 ECHO * Running unit-tests...
 
-ECHO   * Test if BugId can debug cmd.exe...
-CALL "BugId.cmd" %ComSpec% --nApplicationMaxRunTime=1 --cBugId.bEnsurePageHeap=false %*
+ECHO   * Test if BugId cdb debug cmd.exe...
+CALL "BugId.cmd" --verbose %ComSpec% --cBugId.bEnsurePageHeap=false -- /C @ECHO OFF
 IF ERRORLEVEL 1 GOTO :ERROR
+
+ECHO   * Test if BugId can run with redirected output...
+CALL "BugId.cmd" %* >nul
+IF ERRORLEVEL 1 GOTO :ERROR
+
+ECHO   * Test fast mode...
+CALL "BugId.cmd" --fast %ComSpec% --cBugId.bEnsurePageHeap=false -- /C @ECHO OFF >nul
+IF ERRORLEVEL 1 GOTO :ERROR
+
+ECHO   * Test verbose mode...
+CALL "BugId.cmd" --verbose %ComSpec% --cBugId.bEnsurePageHeap=false -- /C @ECHO OFF >nul
+IF ERRORLEVEL 1 GOTO :ERROR
+
+ECHO   * Test if version checks works...
+CALL "BugId.cmd" --version %* >nul
+IF ERRORLEVEL 1 GOTO :ERROR
+
+ECHO   * Test if usage help works...
+CALL "BugId.cmd" --help %* >nul
+IF ERRORLEVEL 1 GOTO :ERROR
+
+ECHO   * Test if internal error reporting works...
+CALL "BugId.cmd" --test-internal-error %* >nul
+IF NOT ERRORLEVEL == 3 GOTO :ERROR
 
 ECHO   * Test if BugId can debug Google Chrome...
 CALL "BugId.cmd" chrome --nApplicationMaxRunTime=10 %*
@@ -18,22 +42,6 @@ IF ERRORLEVEL 1 GOTO :ERROR
 ECHO   * Test if BugId can debug Microsoft Internet Explorer...
 CALL "BugId.cmd" msie --nApplicationMaxRunTime=10 %*
 IF ERRORLEVEL 1 GOTO :ERROR
-
-ECHO   * Test if BugId can run with redirected output...
-CALL "BugId.cmd" %* >nul
-IF ERRORLEVEL 1 GOTO :ERROR
-
-ECHO   * Test if version checks work...
-CALL "BugId.cmd" --version %* >nul
-IF ERRORLEVEL 1 GOTO :ERROR
-
-ECHO   * Test if usage help work...
-CALL "BugId.cmd" --help %* >nul
-IF ERRORLEVEL 1 GOTO :ERROR
-
-ECHO   * Test if internal error reporting works...
-CALL "BugId.cmd" --test-internal-error %* >nul
-IF NOT ERRORLEVEL == 3 GOTO :ERROR
 
 ECHO   * Test if MemGC.cmd can query MemGC settings...
 CALL "MemGC.cmd" ? >nul
