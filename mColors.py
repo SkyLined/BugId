517db0c6b9b5237a8e6c1cac228fa7c09bd8e13f
SkyLined <github@skylined.nl>
2018-02-14 23:20:32 +0100

    delete old colors, add new

--- mColors.py
+++ mColors.py
@@ -3,11 +3,11 @@ from oConsole import oConsole;
 # Colors used in output for various types of information:
 NORMAL =      0x0F07;  # White / Light grey
 DIM =         0x0F08;  # Dark gray / bright black
-HEADER =      0xFF8F;  # Bright White on Dark gray / bright black
-SELECTED =    0xF010;  # Dark blue background
 INFO =        0x0F0A;  # Bright green
 HILITE =      0x0F0F;  # Bright White
 ERROR =       0x0F04;  # Red
-ERROR_INFO =  0x0F0C;  # Bright red
+ERROR_INFO =  0x0F0C;  # Bright Red
+WARNING =     0x0F0E;  # Bright Yellow
+WARNING_INFO = 0x0F0F; # Bright White
 oConsole.uDefaultColor = NORMAL;
 
