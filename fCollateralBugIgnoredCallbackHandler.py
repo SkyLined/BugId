06bc8d0513d2d4737c3d4c212b95793595bdf704
SkyLined <cojmpjum_github.com@sxd.be>
2022-05-20 22:16:31 +0200

    Update fCollateralBugIgnoredCallbackHandler to new mBugId API

--- fCollateralBugIgnoredCallbackHandler.py
+++ fCollateralBugIgnoredCallbackHandler.py
@@ -3,12 +3,19 @@
 from dxConfig import dxConfig;
 from mColorsAndChars import *;
 
-def fCollateralBugIgnoredCallbackHandler(oBugId, sReason):
+def fCollateralBugIgnoredCallbackHandler(oBugId, sInstruction, asActions):
   if not dxConfig["bQuiet"]:
     oConsole.fOutput(
       COLOR_OK, CHAR_OK,
-      COLOR_NORMAL, " This bug was ignored: ",
-      COLOR_INFO, sReason,
-      COLOR_NORMAL, ".",
+      COLOR_NORMAL, " This bug was ignored by skipping the ",
+      COLOR_INFO, sInstruction,
+      COLOR_NORMAL, " instruction:",
     );
+    for uIndex in range(len(asActions)):
+      sAction = asActions[uIndex];
+      oConsole.fOutput(
+        COLOR_NORMAL, "  ",
+        COLOR_LIST, CHAR_LIST,
+        COLOR_NORMAL, " ", sAction, "." if uIndex == len(asActions) - 1 else ",",
+      );
     
