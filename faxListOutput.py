035effb5eeefc9aa61994dedc9fac39f0e932cdf
SkyLined <cojmpjum_github.com@sxd.be>
2022-05-20 22:16:07 +0200

    Update faxListOutput.py

--- faxListOutput.py
+++ faxListOutput.py
@@ -1,8 +1,15 @@
 from mColorsAndChars import *;
 
-def faxListOutput(asData, sAndOr, asImportantData, uImportantDataColor = COLOR_INFO, uNonImportantDataColor = COLOR_DIM, uNormalColor = COLOR_NORMAL):
+def faxListOutput(asData, sAndOr, a0sImportantData = None, uImportantDataColor = COLOR_INFO, uNonImportantDataColor = COLOR_DIM, uNormalColor = COLOR_NORMAL):
+  asData = [str(x) for x in asData];
+  asImportantData = (
+    a0sImportantData if a0sImportantData is not None
+    else asData
+  );
+
   def faxColoredData(sData):
     return [uImportantDataColor if sData in asImportantData else uNonImportantDataColor, sData];
+  
   if len(asData) == 1:
     asOutput = [faxColoredData(asData[0]), uNormalColor];
   elif len(asData) == 2:
