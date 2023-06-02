d5529b18cef83622e42381278bef00ff6c9c037a
SkyLined <cojmpjum_github.com@sxd.be>
2023-06-02 10:40:25 +0200

    mStandardExitCodes

--- mExitCodes.py
+++ mExitCodes.py
@@ -1,13 +1,4 @@
-# Running this script will return an exit code, which are defined here:
-# These are for errors that most of my scripts could have:
-guExitCodeSuccess                        = 0;
-guExitCodeInternalError                  = 1;
-guExitCodeBadArgument                    = 2;
-guExitCodeBadDependencyError             = 3;
-guExitCodeLicenseError                   = 4;
-guExitCodeCannotReadFromFileSystem       = 5;
-guExitCodeCannotWriteToFileSystem        = 6;
-guExitCodeTerminatedByUser               = 7;
+from mStandardExitCodes import *;
 # These are specific to this particular script:
 guExitCodeApplicationBinaryNotFound      = 10;
 guExitCodeNoBugsDetected                 = 11;
---
+++ mStandardExitCodes.py
@@ -0,0 +1,9 @@
+# These error codes are the same for any script that adheres to this standard:
+guExitCodeSuccess                        = 0;
+guExitCodeInternalError                  = 1;
+guExitCodeBadArgument                    = 2;
+guExitCodeBadDependencyError             = 3;
+guExitCodeLicenseError                   = 4;
+guExitCodeCannotReadFromFileSystem       = 5;
+guExitCodeCannotWriteToFileSystem        = 6;
+guExitCodeTerminatedByUser               = 7;
