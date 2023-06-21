147c1e59b159346d42bce89d0f22e38ff3492575
SkyLined <cojmpjum_github.com@sxd.be>
2023-06-21 09:15:23 +0200

    Update tested Python versions

--- BugId.py
+++ BugId.py
@@ -115,7 +115,7 @@ try:
       fTerminate(guExitCodeBadArgument);
   
   if __name__ == "__main__":
-    asTestedPythonVersions = ["3.8.5", "3.9.1", "3.9.7", "3.10.0", "3.11.1"];
+    asTestedPythonVersions = ["3.8.5", "3.9.1", "3.9.7", "3.10.0", "3.11.1", "3.11.4"];
     
     gasAttachForProcessExecutableNames = [];
     gasLowercaseBinaryNamesThatAreAllowedToRunWithoutPageHeap = [
