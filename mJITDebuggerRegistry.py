f110ff0da3767b08b4717015dae5e6dd4da9c55d
SkyLined <cojmpjum_github.com@sxd.be>
2024-04-08 23:05:45 +0200

    Generalize code to use a loop rather than have two slightly different copies in the same function.

--- mJITDebuggerRegistry.py
+++ mJITDebuggerRegistry.py
@@ -1,3 +1,12 @@
-sComandLineHiveName = "HKLM";
-sComandLineKeyPath = r"Software\Microsoft\Windows NT\CurrentVersion\AEDebug";
-sComandLineKeyPathx86 = r"SOFTWARE\WOW6432Node\Microsoft\Windows NT\CurrentVersion\AeDebug";
+sCommandLineHiveName = "HKLM";
+ddsCommandLineKeyPath_by_sTargetBinaryISA_by_sOSISA = {
+  "x64": {
+    # Two different registry paths must be adjusted individually on x64 systems - one for 64-bit applications, and a different one for 32-bit 
+    # applications, as outlined in https://learn.microsoft.com/en-us/windows-hardware/drivers/debugger/enabling-postmortem-debugging
+    "x64": r"Software\Microsoft\Windows NT\CurrentVersion\AEDebug",
+    "x86": r"SOFTWARE\WOW6432Node\Microsoft\Windows NT\CurrentVersion\AeDebug",
+  },
+  "x86": {
+    "x86": r"Software\Microsoft\Windows NT\CurrentVersion\AEDebug",
+  },
+};
